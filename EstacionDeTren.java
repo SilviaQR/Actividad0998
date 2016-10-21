36be51d9ea2bf237e9187d20eaf479faeed88c15
Silvia <silvia.quintanar.robles@gmail.com>
2016-10-21 13:43:09 +0200

    0178.a Traducido a español, corregido y compilado.

---
+++ EstacionDeTren.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class TrainStation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EstacionDeTren
+{
+    // instance variables - replace the example below with your own
+    private MaquinaDeBilletes maquina1;
+    private MaquinaDeBilletes maquina2;
+    private int dineroTotal;
+
+    /**
+     * Constructor for objects of class TrainStation
+     */
+    public EstacionDeTren()
+    {
+        // initialise instance variables
+        maquina1 = new MaquinaDeBilletes(300);
+        maquina2 = new MaquinaDeBilletes(200);
+        dineroTotal = 0;
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void imprimirDineroTotal()
+    {
+        // put your code here
+        dineroTotal = maquina1.getTotal();
+        dineroTotal = dineroTotal + maquina2.getTotal();
+        System.out.println(dineroTotal);
+    }
+}
